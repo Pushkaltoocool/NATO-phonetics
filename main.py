dda29f4fb10a651467e3c2f12dba95b4e9d81489
Pushkal Vashist <118830976+Pushkaltoocool@users.noreply.github.com>
2025-03-08 22:21:37 +0800

    add exception handling

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ letters_dataframe = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 letters_dict = {row.letter:row.code for (index, row) in letters_dataframe.iterrows()}
 
-word = input("What is your word: ").upper()
 
-words_list = [letters_dict[letter] for letter in word]
-print(words_list)
+
+def give_list():
+    word = input("What is your word: ").upper()
+    words_list = [letters_dict[letter] for letter in word]
+    print(words_list)
+
+
+try:
+    give_list()
+except:
+    print("Please includes alphabets only")
+    give_list()
+
+
